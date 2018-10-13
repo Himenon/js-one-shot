c552642a9412848efe84533f1e4d4847d15e9ec8
Himenon <k.himeno314@gmail.com>
2018-10-14 02:40:13 +0900

    update(add): new Functionを用いたReact.createElementの実験

--- src/react-function/__tests__/oneshot.test.tsx
+++ src/react-function/__tests__/oneshot.test.tsx
@@ -1,6 +1,46 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 import * as OneShot from '../oneshot';
 
-test.skip('StringからFunctionを生成する', () => {
-  const maker = OneShot.createComponent();
-  expect(maker()).toEqual(OneShot.MyComponent);
+test('React.MyComponent', () => {
+  const comp = React.createElement(OneShot.HogeComponent, { text: 'hoge' });
+  expect(comp).toEqual(<OneShot.HogeComponent text='hoge' />);
 });
+
+test('HogeCreatorSeedのテスト', () => {
+  const hogeCreatorSeed = OneShot.getHogeCreatorSeed();
+  const keys = [OneShot.HogeComponent];
+  const receiveComponent = hogeCreatorSeed(React, ...keys)();
+  const expectComponent = <OneShot.HogeComponent text='hoge' />;
+  const myComponent1 = renderer.create(receiveComponent);
+  const myComponent2 = renderer.create(expectComponent);
+  expect(myComponent1.toJSON()).toEqual(myComponent2.toJSON());
+});
+
+test('HogeCreatorのテスト', () => {
+  const hogeCreator = OneShot.getHogeCreator();
+  const receiveComponent = hogeCreator();
+  const expectComponent = <OneShot.HogeComponent text='hoge' />;
+  const myComponent1 = renderer.create(receiveComponent);
+  const myComponent2 = renderer.create(expectComponent);
+  expect(myComponent1.toJSON()).toEqual(myComponent2.toJSON());
+})
+
+
+test('childrenを含むテスト', () => {
+  const nextComponentCreator = OneShot.getNestComponentCreator();
+  const receiveComponent = nextComponentCreator();
+  const expectComponent = <OneShot.FooComponent name="foooo"><OneShot.HogeComponent text="hoge" /></OneShot.FooComponent>;
+  const myComponent1 = renderer.create(receiveComponent);
+  const myComponent2 = renderer.create(expectComponent);
+  expect(myComponent1.toJSON()).toEqual(myComponent2.toJSON());
+})
+
+test('引数を取得するような生成パターン', () => {
+  const nextComponentCreator = OneShot.getNestComponentCreator2({ name: "foooo" }, { text: "hoge" });
+  const receiveComponent = nextComponentCreator();
+  const expectComponent = <OneShot.FooComponent name="foooo"><OneShot.HogeComponent text="hoge" /></OneShot.FooComponent>;
+  const myComponent1 = renderer.create(receiveComponent);
+  const myComponent2 = renderer.create(expectComponent);
+  expect(myComponent1.toJSON()).toEqual(myComponent2.toJSON());
+})
--- src/react-function/oneshot.tsx
+++ src/react-function/oneshot.tsx
@@ -1,15 +1,61 @@
 import * as React from 'react';
 
-export interface MyComponentProps {
+export interface HogeProps {
   text: string;
 }
 
-export class MyComponent extends React.Component<MyComponentProps, {}> {
+export interface FooProps {
+  name: string;
+}
+
+export class HogeComponent extends React.Component<HogeProps, {}> {
   public render() {
     return <div>${this.props.text}</div>;
   }
 }
 
-export function createComponent() {
-  return new Function('React', 'MyComponent', `return props => ${MyComponent}`);
+export class FooComponent extends React.Component<FooProps, {}> {
+  public render() {
+    return <div>${this.props.name}{this.props.children}</div>;
+  }
+}
+
+/**
+ * StringからReactComponentを作成する
+ */
+export function getHogeCreatorSeed() {
+  const args = ['MyComponent'];
+  const func = 'React.createElement(MyComponent, { text: "hoge" }, null)';
+  const create = new Function('React', ...args, `return props => ${func}`);
+  return create;
+}
+
+/**
+ * getHogeCreatorSeed の簡略版
+ */
+export function getHogeCreator() {
+  const args = ['MyComponent'];
+  const func = 'React.createElement(MyComponent, { text: "hoge" }, null)';
+  const create = new Function('React', ...args, `return props => ${func}`);
+  return create(React, HogeComponent);
+}
+
+/**
+ * 階層構造のあるコンポーネントのファクトリーの作成
+ */
+export function getNestComponentCreator() {
+  const args = ['FooComponent', 'HogeComponent'];
+  const func = `React.createElement(FooComponent, { name: "foooo" }, React.createElement(HogeComponent, { text: "hoge" }, null))`;
+  const create = new Function('React', ...args, `return props => ${func}`);
+  return create(React, FooComponent, HogeComponent);
+}
+
+/**
+ * 引数を取得するような生成パターン
+ */
+export function getNestComponentCreator2(props1: FooProps, props2: HogeProps) {
+  const args = ['FooComponent', 'HogeComponent'];
+  const func = `React.createElement(FooComponent, ${JSON.stringify(props1)}, React.createElement(HogeComponent, ${JSON.stringify(props2)}, null))`;
+  const create = new Function('React', ...args, `return props => ${func}`);
+  return create(React, FooComponent, HogeComponent);
 }
